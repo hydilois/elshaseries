e6bf86965c9f3c52ae64f56977d7195584c82abc
Hydil Aicard Sokeing <shydilaicard@gmail.com>
2022-02-28 16:02:52 +0100

    Add the translation feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portofolio';
+  constructor(public translate:TranslateService) {
+    translate.addLangs(['fr', 'en'])
+    translate.setDefaultLang('fr')
+  }
+
+  switchLanguage(lang: string){
+    this.translate.use(lang);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { HttpClient, HttpClientModule  } from "@angular/common/http";
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +12,21 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function httpTranslateLoader(http:HttpClient){
+  return new TranslateHttpLoader(http)
+}
